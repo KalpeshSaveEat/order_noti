fc7d9afa4a075c0e96d280a21e741cb1bfb3d52c
KalpeshWani <kalpesh.wani@saveeat.in>
2024-03-04 10:05:09 +0530

    adding origin

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "*",
+    origin: "https://order-noti.vercel.app",
   },
 })
 
