e9cef922554d90cd1a23433352a922473444d317
KalpeshWani <kalpesh.wani@saveeat.in>
2024-03-04 10:54:29 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,10 @@ const server = app.listen(PORT, () => {
 //socket io config
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
+
   cors: {
-    origin: "*",
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
   },
 })
 
